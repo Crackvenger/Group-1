2537ca0fadce199a5954bce1d415d18cfac6d705
Crackvenger <tsukinome123@gmail.com>
2019-12-15 06:45:22 +0800

    Updated my pages and fixed linking issues with content.html
    Added a bit of javscript and fixed the links.

---
+++ js/script.js
@@ -0,0 +1,5 @@
+function Click()
+{
+  var x = document.getElementById("secret");
+  x.innerHTML = "We are a group of beginner programmers who are trying our best. Our members are Boiy, Ferri, Aca, and myself Alcon";
+}
